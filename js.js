6e24ec2047d515cd4e4a8cd156a8bdd5739b89c3
Shandy <ambrosia.intake@gmail.com>
2021-03-25 12:24:35 +0000

    win logic and reset mostly working

--- js.js
+++ js.js
@@ -1,26 +1,25 @@
 const board = (() => {
-    let positions = [ [0,0,0],
-                      [0,0,0],
-                      [0,0,0] ]
-    const cols = [ [positions[0][0], positions[1][0], positions[2][0]],
-                   [positions[0][1], positions[1][1], positions[2][1]],
-                   [positions[0][2], positions[1][2], positions[2][2]]                
-                 ]
-
-    const diags = [ [positions[0][0], positions[1][1], positions[2][2]],
-                    [positions[0][2], positions[1][1], positions[2][0]]        
-                  ]
-    return {positions,cols,diags};
+    const positions = [ 0,0,0,
+                        0,0,0,
+                        0,0,0 ]
+    const winConditions = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [3,4,5],
+        [6,7,8]
+    ]
+    return {positions,winConditions};
 })();
 
 const display = (() => {
-    const gameDiv = document.querySelector("board");
     const squares =  document.querySelectorAll('.move');
     const draw = () => {
         for(let i=0;i<9;i++) {
-            if(board.positions[i]) {
-                squares[i].innerHTML = "<span>"+board.positions[i]+"</span>";
-            }
+            squares[i].innerHTML = board.positions[i] ? "<span>"+board.positions[i]+"</span>" : "";
         }
     }
     const player = (team) => {
@@ -32,38 +31,105 @@ const display = (() => {
 
 const play = (() => {
     let currentPlayer;
+    let winState = 0;
+    let xPlayer;
+    let oPlayer;
+    let winMsg;
+    let isWinner;
+    const main = document.querySelector("main");
     const init = () => {
-        randomize = Math.floor(Math.random() * 2);
-        currentPlayer = (randomize) ? xPlayer : oPlayer;
-        display.player(currentPlayer.team);
-        display.draw();
-        for(let i=0;i<9;i++) {
-            display.squares[i].addEventListener('click', (e) => {
-                play.turn(i);
-            }); 
-        }
+        const startBtn = document.querySelector("#makeNames");
+        const inputDiv = document.querySelector("#nameInputs");
+        inputDiv.style.display = "flex";
+        startBtn.addEventListener('click', () => {
+            const xName = document.querySelector("#xplayer").value;
+            const oName = document.querySelector("#oplayer").value;
+            if (xName && oName) {
+                xPlayer = makePlayer('X', xName);
+                oPlayer = makePlayer('O', oName);
+                randomize = Math.floor(Math.random() * 2);
+                currentPlayer = (randomize) ? xPlayer : oPlayer;
+                display.player(currentPlayer.name);
+                inputDiv.style.display = "none";
+                display.draw();
+                for(let i=0;i<9;i++) {
+                    display.squares[i].addEventListener('click', (e) => {
+                        play.turn(i);
+                    }); 
+                }
+            }  
+        });
+        
     }
     const turn = (location) => {
         if (!board.positions[location]) {
             board.positions[location] = currentPlayer.team;
+            /* Update each win condition position to match player marker */
+            for(let i=0;i<board.winConditions.length;i++) {
+                let set = board.winConditions[i];
+                for(let n=0;n<3;n++) {
+                    if (set[n] == location) {
+                        set[n] = currentPlayer.team;
+                    }
+                }
+                /* If a win condition is met then declare a winner */
+                isWinner = set => set.every(v => v !== 0 && v === set[0]);
+                if(isWinner(set)) {
+                    winState = 1;
+                    display.draw();
+                    endGame(winState);
+                }
+            }
+            /* Check for tie */
+            if(board.positions.every(position => position !== 0) && winState < 1) {
+                winState = 2;
+                display.draw();
+                endGame(winState);
+            }
             display.draw();
             currentPlayer == xPlayer ? currentPlayer = oPlayer : currentPlayer = xPlayer;
-            display.player(currentPlayer.team);
-            analyze(board.positions);
-        } 
+            display.player(currentPlayer.name);
+        }
     }
-    const analyze = (board) => {
-        board.forEach(element =>
-            console.log(element));
+    const endGame = (winState) => {
+        for(let i=0;i<9;i++) {
+            display.squares[i].removeEventListener('click', (e) => {
+                play.turn(i);
+            }); 
+        }
+        winMsg = document.createElement('h3');
+        const resetBtn = document.createElement('button');
+        resetBtn.textContent = "Reset";
+        resetBtn.addEventListener('click', () => {
+            board.winConditions = [
+                [0,1,2],
+                [0,3,6],
+                [0,4,8],
+                [1,4,7],
+                [2,5,8],
+                [2,4,6],
+                [3,4,5],
+                [6,7,8]
+            ]
+            board.positions = [0,0,0,0,0,0,0,0,0];
+            display.draw();
+            winState = 0;
+            xPlayer = {};
+            oPlayer = {};
+            currentPlayer = {};
+            isWinner = 0;
+            main.removeChild(winMsg);
+            play.init();
+        });
+        winState == 2 ? winMsg.textContent = "It's a tie!" : winMsg.textContent = currentPlayer.name+" wins!";
+        winMsg.appendChild(resetBtn);
+        main.appendChild(winMsg);
     }
-    return {init,turn,currentPlayer:currentPlayer};
+    return {init,turn,currentPlayer:currentPlayer,winState:winState};
 })();
 
-const makePlayer = (team) => {
-    return {team};
+const makePlayer = (team,name) => {
+    return {team,name};
 }
-
-const xPlayer = makePlayer('X');
-const oPlayer = makePlayer('O');
 
 play.init();
